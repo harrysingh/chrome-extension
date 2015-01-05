2544620509f40a1a466031809bbacf09e18eb6ee
Sachin <sacjain@adobe.com>
2015-01-05 15:37:08 +0530

    Issue #22 Background working of headers (Add/Remove/Modify in Request and Response

--- src/background/background.js
+++ src/background/background.js
@@ -51,46 +51,122 @@ BG.Methods.matchUrlWithReplaceRulePairs = function(rule, url) {
   return resultingUrl;
 };
 
+BG.Methods.removeHeader = function(headers, name) {
+  for (var i = headers.length - 1; i >= 0; i--) {
+    if (headers[i].name.toLowerCase() === name.toLowerCase()) {
+      headers.splice(i, 1);
+      break;
+    }
+  }
+};
+
+BG.Methods.modifyHeaderIfExists = function(headers, newHeader) {
+  for (var i = headers.length - 1; i >= 0; i--) {
+    if (headers[i].name.toLowerCase() === newHeader.name.toLowerCase()) {
+      headers[i].value = newHeader.value;
+      break;
+    }
+  }
+};
+
+BG.Methods.modifyHeaders = function(originalHeaders, headersTarget) {
+  var rule,
+    headerPairs,
+    modification;
+
+  for (var i = 0; i < StorageService.records.length; i++) {
+    rule = StorageService.records[i];
+
+    if (rule.status !== RQ.RULE_STATUS.ACTIVE || rule.ruleType !== RQ.RULE_TYPES.HEADERS) {
+      continue;
+    }
+
+    headerPairs = rule.pairs || [];
+
+    for (var headerIndex = 0; headerIndex < headerPairs.length; headerIndex++) {
+      modification = headerPairs[headerIndex];
+
+      if (modification.target !== headersTarget || !modification.header) {
+        continue;
+      }
+
+      switch (modification.type) {
+        case RQ.MODIFICATION_TYPES.ADD:
+          originalHeaders.push({name: modification.header, value: modification.value});
+          break;
+
+        case RQ.MODIFICATION_TYPES.REMOVE:
+          BG.Methods.removeHeader(originalHeaders, modification.header);
+          break;
+
+        case RQ.MODIFICATION_TYPES.MODIFY:
+          BG.Methods.modifyHeaderIfExists(originalHeaders, {
+            name: modification.header,
+            value: modification.value
+          });
+      }
+    }
+  }
+
+  return originalHeaders;
+};
+
+BG.Methods.modifyUrl = function(details) {
+  for (var i = 0; i < StorageService.records.length; i++) {
+    var rule = StorageService.records[i];
+
+    if (rule.status !== RQ.RULE_STATUS.ACTIVE) {
+      continue;
+    }
+
+    switch(rule.ruleType) {
+      case RQ.RULE_TYPES.REDIRECT:
+        if (BG.Methods.matchUrlWithRule(rule, details.url)) {
+          return { redirectUrl: rule.destination };
+        }
+        break;
+
+      /**
+      * In case of Cancel Request, destination url is 'javascript:'
+      */
+      case RQ.RULE_TYPES.CANCEL:
+        if (BG.Methods.matchUrlWithRule(rule, details.url)) {
+          return { redirectUrl: 'javascript:' };
+        }
+        break;
+
+      case RQ.RULE_TYPES.REPLACE:
+        var resultingUrl = BG.Methods.matchUrlWithReplaceRulePairs(rule, details.url);
+        if (resultingUrl !== null) {
+          return { redirectUrl: resultingUrl };
+        }
+        break;
+    }
+  }
+};
+
 BG.Methods.registerListeners = function() {
   chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      for (var i = 0; i < StorageService.records.length; i++) {
-        var rule = StorageService.records[i];
-
-        if (rule.status !== RQ.RULE_STATUS.ACTIVE) {
-          continue;
-        }
-
-        switch(rule.ruleType) {
-          case RQ.RULE_TYPES.REDIRECT:
-            if (BG.Methods.matchUrlWithRule(rule, details.url)) {
-              return { redirectUrl: rule.destination };
-            }
-            break;
-
-          /**
-            * In case of Cancel Request, destination url is 'javascript:'
-            */
-          case RQ.RULE_TYPES.CANCEL:
-            if (BG.Methods.matchUrlWithRule(rule, details.url)) {
-              return { redirectUrl: 'javascript:' };
-            }
-            break;
-
-          case RQ.RULE_TYPES.REPLACE:
-            var resultingUrl = BG.Methods.matchUrlWithReplaceRulePairs(rule, details.url);
-            if (resultingUrl !== null) {
-              return { redirectUrl: resultingUrl };
-            }
-            break;
-        }
-      }
-    },
-    {
-      urls: ["<all_urls>"]
-    },
-    ["blocking"]
+    BG.Methods.modifyUrl, { urls: ['<all_urls>'] }, ['blocking']
   );
+
+  chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    var modifiedHeaders = BG.Methods.modifyHeaders(details.requestHeaders, RQ.HEADERS_TARGET.REQUEST);
+    return { requestHeaders: modifiedHeaders };
+  },
+  { urls: ['<all_urls>'] },
+  ['blocking', 'requestHeaders']);
+
+  chrome.webRequest.onHeadersReceived.addListener(function(details) {
+    var modifiedHeaders = BG.Methods.modifyHeaders(details.responseHeaders, RQ.HEADERS_TARGET.RESPONSE);
+    /*
+    if (details.url == 'https://developer.chrome.com/extensions/webRequest') {
+      console.log('sachin');
+    }*/
+    return { responseHeaders: modifiedHeaders };
+  },
+  { urls: ['<all_urls>'] },
+  ['blocking', 'responseHeaders']);
 };
 
 chrome.browserAction.onClicked.addListener(function () {
