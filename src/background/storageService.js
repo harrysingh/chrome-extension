8f759568186af03e45fcb1df3a07b9b7ff541642
Sachin <sacjain@adobe.com>
2015-08-17 17:46:21 +0530

    Issue #50 [6] Update Storage Service to avoid caching when object has avoidCache set to true

--- src/background/storageService.js
+++ src/background/storageService.js
@@ -1,3 +1,10 @@
+/**
+ * Wrapper over Chrome Storage Service APIs.
+ * TODO: Convert Storage Service into a Class and accept type of storage as param.
+ * TODO: Move this into another repo so that people should re-use it.
+ * TODO: Usage: var localStorageService = new StorageService({ DB: 'local' });
+ */
+
 var StorageService = {
   records: [],
   isRecordsFetched: false,
@@ -56,46 +63,58 @@ StorageService.removeRecord = function(key, callback) {
   StorageService.DB.remove(key, callback);
 };
 
+StorageService.getCachedRecordIndex = function(keyToFind) {
+  var recordKey,
+    recordIndex;
+
+  for (recordIndex = 0; recordIndex < StorageService.records.length; recordIndex++) {
+    recordKey = StorageService.records[recordIndex].id;
+
+    if (recordKey === keyToFind) {
+      return recordIndex;
+    }
+  }
+
+  return -1;
+};
+
+/**
+ * StorageService.records are updated on every add/edit/delete operation
+ * So that background rules can be updated which are executed when each request URL is intercepted
+ * @param changes SuperObject with key as Object key which is changed with old and new values
+ * @param namespace Storage type: 'sync' or 'local'
+ */
 StorageService.updateRecords = function(changes, namespace) {
+  var changedObject,
+    changedObjectIndex,
+    objectExists,
+    changedObjectKey;
+
   if (StorageService.DB === chrome.storage[namespace]) {
-    for (changedObjectkey in changes) {
-      var change = changes[changedObjectkey],
-        objectExists = false,
-        i,
-        recordKey;
-
-      /*
-       StorageService.records are updated on every add/edit/delete operation
-       So that background rules can be updated which are executed when each request URL is intercepted
-       */
-
-      /* Add/Edit Rule operation */
-      if (typeof change.oldValue === 'undefined' && typeof change.newValue !== 'undefined') {
-        for (i = 0; i < StorageService.records.length; i++) {
-          recordKey = StorageService.records[i].id || (StorageService.records[i].ruleType + '_' + StorageService.records[i].creationDate);
-
-          if (recordKey === changedObjectkey) {
-            StorageService.records[i] = change.newValue;
-            objectExists = true;
-            break;
-          }
-        }
-
-        if (!objectExists) {
-          StorageService.records.push(change.newValue);
-        }
+    for (changedObjectKey in changes) {
+      if (!changes.hasOwnProperty(changedObjectKey)) {
+        continue;
       }
 
-      /* Delete Rule Operation */
-      if (typeof change.oldValue !== 'undefined' && typeof change.newValue === 'undefined') {
-        for (i = 0; i < StorageService.records.length; i++) {
-          recordKey = StorageService.records[i].id || (StorageService.records[i].ruleType + '_' + StorageService.records[i].creationDate);
+      changedObject = changes[changedObjectKey];
+      changedObjectIndex = this.getCachedRecordIndex(changedObjectKey);
+      objectExists = (changedObjectIndex !== -1);
 
-          if (recordKey === changedObjectkey) {
-            StorageService.records = StorageService.records.splice(i, 1);
-            break;
-          }
+      // Add/Edit Rule operation
+      if (typeof changedObject.newValue !== 'undefined') {
+        // Don't cache records which explicitly ask not to cache them
+        if (changedObject.newValue['avoidCache'] === true) {
+          continue;
         }
+
+        objectExists
+          ? StorageService.records[changedObjectIndex] = changedObject.newValue    /* Update existing object (Edit) */
+          :  StorageService.records.push(changedObject.newValue);                  /* Create New Object */
+      }
+
+      // Delete Rule Operation
+      if (typeof changedObject.oldValue !== 'undefined' && typeof changedObject.newValue === 'undefined' && objectExists) {
+        StorageService.records.splice(changedObjectIndex, 1);
       }
     }
   }
