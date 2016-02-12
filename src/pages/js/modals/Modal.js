fc45fc9f3675dfe735c2db8d256044bddb3b7d34
Sachin <sacjain@adobe.com>
2016-02-12 08:44:09 +0530

    Issue #93: Save shared listName to firebase

--- src/pages/js/modals/Modal.js
+++ src/pages/js/modals/Modal.js
@@ -8,10 +8,15 @@ var Modal = BaseView.extend({
   },
 
   events: {
+    'change input[data-key]': 'updateValueFromInput',
     'click .cta-container .btn-primary': 'handlePrimaryButtonClicked'
   },
 
   handlePrimaryButtonClicked: function() { /* No Op */ },
+
+  updateValueFromInput: function(event) {
+    this.model.set(event.target.getAttribute('data-key'), event.target.value);
+  },
 
   show: function(options) {
     RQ.showModalView(this, options);
